52d0048d9c8f20f67ce9d786c016c4bd252228ea
Jeremi <jeremi_P@op.pl>
2026-05-26 17:15:14 +0200

    wersja ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import pytania from './pytania';
 
 function App() {
+  const [odp, setOdp] = useState('');
+  const [pytanie, setPytanie] = useState('');
+  const [odpowiedz, setOdpowiedz] = useState('');
+  const [czyPoprawne, setCzyPoprawne] = useState(null);
+  const [Hp, setHP] = useState(3)
+  const [punkty, setPunkty] = useState(0);
+
+  const losuj = () => {
+    const nrPytania = Math.floor(Math.random() * pytania.length);
+    const obecnePytanie = pytania[nrPytania];
+
+    setOdp(obecnePytanie.odp);
+    setPytanie(obecnePytanie.pytanie);
+    setOdpowiedz('');
+  };
+
+  const sprawdzOdpowiedz = () => {
+    const wpisanaOdpowiedz = odpowiedz.trim().toLowerCase();
+    const poprawnaOdpowiedz = odp.trim().toLowerCase();
+
+    if (wpisanaOdpowiedz === poprawnaOdpowiedz) {
+      setCzyPoprawne(true);
+      setPunkty((p) => p + 1);
+      losuj();
+    } else {
+      setCzyPoprawne(false);
+      setHP(h => h-1)
+    }
+  };
+  const zapisz = () => {
+    localStorage.setItem("iloscpunktow", punkty )
+   }
+   const zaladuj = () => {
+    setPunkty(p => localStorage.getItem("iloscpunktow"))
+   }
+
+  useEffect(() => {
+    losuj();
+  }, []);
+
+  const serca = [];
+  for (let i = 0; i < Hp; i++) {
+    serca.push(<img key={i} src="/serce.svg" alt="serce" className="serce" />);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {serca}
+      <h1 className="tytl">QUIZ NA DZIEŃ MAMY</h1>
+      <h2>{pytanie}</h2>
+      <h3>Punkty: {punkty}</h3>
+      <input
+        value={odpowiedz}
+        onChange={(e) => setOdpowiedz(e.target.value)}
+      />
+      <button onClick={sprawdzOdpowiedz}>odpowiedz</button>
+      <button onClick={zapisz}>zapisz</button>
+      <button onClick={zaladuj}>zaladuj</button>
+
+      
+      {czyPoprawne === true && <p>Dobrze!</p>}
+      {czyPoprawne === false && <p>Spróbuj jeszcze raz.</p>}
     </div>
   );
 }
---
+++ src/pytania.jsx
@@ -0,0 +1,64 @@
+const pytania = [
+  {
+    pytanie:'Gdzie uważasz, że twoi synowie chcieliby z tobą pójść? A) do zoo B) do Empiku C) Pixel XL D) do muzeum',
+    odp: 'Pixel XL',
+  },
+  {
+    pytanie: 'Jaka jest ulubiona postać Jeremiego z Modern Family?',
+    odp: 'Joe',
+  },
+  {
+    pytanie: 'Jaką serię książek Cyryl lubi najbardziej?',
+    odp: 'Harry Potter',
+  },
+  {
+    pytanie: 'W jaką grę planszową najbardziej lubi grać Jeremi? Podaj skrót.',
+    odp: 'Władca Pierścieni',
+  },
+  {
+    pytanie: 'Jaki jest obecnie ulubiony film Jeremiego?',
+    odp: 'Avatar',
+  },
+  {
+    pytanie: 'Jaki jes ulubiony przedmiot jeremiego :P',
+    odp: 'Informatyka',
+  },
+  {
+    
+    pytanie: 'jakie jest ulubione jedzenie cyryla',
+    odp: 'sushi',
+  },
+  {
+    pytanie: 'jaki jest ulubiony przedmiot cyryla',
+    odp: 'matematyka',
+  },
+  {
+    
+    pytanie: 'jaki jest ulubiony kolor jeremiego',
+    odp: 'zielony',
+  },
+{
+    
+    pytanie: 'jaki jest ulubiony smak lodów cyryla',
+    odp: 'śmietankowy',
+},
+{
+    
+    pytanie: 'jaką markę samochodów jeremi lubi najbardziej ',
+    odp: 'ferari',
+},
+{
+    
+    pytanie: 'jaki procesor ma jeremi w Pc',
+    odp: 'AMD 7 7700',
+},
+{
+    
+    pytanie: 'jakie jest ulubione jedzenie jeremiego',
+    odp: 'Pizza',
+},
+{
+    
+    pytanie: '',
+    odp: 'Informatyka',
+}]
